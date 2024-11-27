ac0bf478d56a147d9eb3fdb2bc95bf958d9fecfb
PutraAdam08 <putraadam322@gmail.com>
2024-11-27 16:37:15 +0700

    add SceneInit class

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as THREE from 'three'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SceneInit from "./lib/SceneInit.js"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const test = new SceneInit("myTreeJsCanvas"); //SeceneInit class to simplify the main app.js
+    test.initialize();
+    test.animate();
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    test.scene.add(boxMesh);
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas id="myTreeJsCanvas"/>
     </>
   )
 }
