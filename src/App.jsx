276419c98a703d886178bae728e52a89cef61c6c
PutraAdam08 <putraadam322@gmail.com>
2024-12-07 12:59:02 +0700

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,64 @@
 import { useState, useEffect } from 'react'
 import * as THREE from 'three'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import './App.css'
 import SceneInit from "./lib/SceneInit.js"
 
 
+
 function App() {
   useEffect(() => {
-    const test = new SceneInit("myTreeJsCanvas"); //SeceneInit class to simplify the main app.js
+    const test = new SceneInit('myThreeJsCanvas');
     test.initialize();
     test.animate();
+
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('/hall.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      // console.log(loadedModel);
+      
+
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });/* */
+
+    {
+      let spotLight = new THREE.SpotLight( 0xff8800, 6, 10, Math.PI / 16, .02, 2 )
+      // let spotLight = new THREE.SpotLight( 0xff8800, 1, 10, Math.PI / 16, 0, 2 )
+      spotLight.position.set( 2, 7, 0 )
+      let target = spotLight.target //= new THREE.Object3D()
+      test.scene.add( target )
+      target.position.set( 0, 5, 0 )
+      spotLight.castShadow = true
+      test.scene.add( spotLight )
+      // spotLight.shadow.radius = 0
+  }
+
+    /*
     const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMaterial = new THREE.MeshStandardMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    const planeGeometry = new THREE.PlaneGeometry(32, 32);
+    const planeMaterial = new THREE.MeshStandardMaterial();
+    const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)/* */
 
     test.scene.add(boxMesh);
+    test.scene.add(planeMesh);/**/
   }, [])
 
   return (
     <>
-      <canvas id="myTreeJsCanvas"/>
+      <canvas id="myThreeJsCanvas"/>
     </>
   )
 }
--- src/lib/SceneInit.js
+++ src/lib/SceneInit.js
@@ -3,12 +3,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import RenderPixelatedPass from './RenderPixelatedPass';
 import PixelatePass from './PixelatePass';
-import { UnrealBloomPass } from 'three/examples/jsm/Addons.js';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { GreaterEqualDepth, Vector2 } from "three"
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { GlitchPass } from 'three/addons/postprocessing/GlitchPass.js';
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+import { Vector2 } from "three"
 
 export default class SceneInit {
   constructor(canvasId) {
@@ -16,7 +13,7 @@ export default class SceneInit {
     this.scene = undefined;
     this.camera = undefined;
     this.renderer = undefined;
-  
+
     this.composer = undefined;
 
     // NOTE: Camera params;
@@ -41,38 +38,28 @@ export default class SceneInit {
     renderResolution.x |= 3
     renderResolution.y |= 3
     let aspectRatio = screenResolution.x / screenResolution.y
-    
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x0c0523);
     this.camera = new THREE.PerspectiveCamera(
       this.fov,
       window.innerWidth / window.innerHeight,
       1,
       1000
     );
-    this.camera.position.z = 48;
+    this.camera.position.y = 9;
+    this.camera.position.z = 36;
 
     // NOTE: Specify a canvas which is already created in the HTML.
     const canvas = document.getElementById(this.canvasId);
     this.renderer = new THREE.WebGLRenderer({
       canvas,
       // NOTE: Anti-aliasing smooths out the edges.
-      antialias: false,
+      antialias: true,
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     // this.renderer.shadowMap.enabled = true;
     document.body.appendChild(this.renderer.domElement);
 
     this.composer = new EffectComposer( this.renderer )
-    /*const renderPass = new RenderPass( this.scene, this.camera );
-    this.composer.addPass( renderPass );
-
-    const glitchPass = new GlitchPass();
-    this.composer.addPass( glitchPass );
-
-    const outputPass = new OutputPass();
-    this.composer.addPass( outputPass );*/
-    // composer.addPass( new RenderPass( scene, camera ) )
     this.composer.addPass( new RenderPixelatedPass( renderResolution, this.scene, this.camera ) )
     let bloomPass = new UnrealBloomPass( screenResolution, .4, .1, .9 )
     this.composer.addPass( bloomPass )
@@ -84,14 +71,13 @@ export default class SceneInit {
     document.body.appendChild(this.stats.dom);
 
     // ambient light which is for the whole scene
-    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    this.ambientLight.castShadow = true;
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 2);
     this.scene.add(this.ambientLight);
 
     // directional light - parallel sun rays
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.directionalLight = new THREE.DirectionalLight(0xfafafa, 2);
     // this.directionalLight.castShadow = true;
-    this.directionalLight.position.set(0, 32, 64);
+    this.directionalLight.position.set(0, 10, 10);
     this.scene.add(this.directionalLight);
 
     // if window resizes
@@ -122,7 +108,7 @@ export default class SceneInit {
     // NOTE: Update uniform data on each render.
     // this.uniforms.u_time.value += this.clock.getDelta();
     this.renderer.render(this.scene, this.camera);
-  }*/
+  }/* */
 
   onWindowResize() {
     this.camera.aspect = window.innerWidth / window.innerHeight;
